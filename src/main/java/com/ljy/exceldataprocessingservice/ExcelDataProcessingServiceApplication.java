92200d595cea79822b7a37609618676fa49353c0
loty <jylee04@trenbe.com>
2022-06-26 16:07:44 +0900

    프로젝트 세팅

---
+++ src/main/java/com/ljy/exceldataprocessingservice/ExcelDataProcessingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ljy.exceldataprocessingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelDataProcessingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelDataProcessingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ljy/exceldataprocessingservice/ExcelDataProcessingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljy.exceldataprocessingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelDataProcessingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
